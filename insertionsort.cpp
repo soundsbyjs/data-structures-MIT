68242d87764a172f25df4e17f8bdd9f9dfa690f2
soundsbyjs <85450032+soundsbyjs@users.noreply.github.com>
2022-01-25 14:55:16 -0500

    Add files via upload

---
+++ insertionsort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <array>
+
+int main()
+{
+	std::array<int, 1000> arrNum;
+
+	for(int i = 0; i < arrNum.size(); i++) arrNum[i] = rand() % 50;
+	for(int i = 0; i < arrNum.size(); i++) std::cout<<arrNum[i]<<std::endl;
+	
+	
+	for(int i = 1; i < arrNum.size(); i++)	
+	{
+		int key = arrNum[i];
+		int index = i - 1;
+		while(index >= 0 && arrNum[index] > key)
+		{
+			arrNum[index + 1] = arrNum[index];
+			index--;
+		}
+		arrNum[index + 1] = key;
+	}
+	std::cout<<std::endl;
+	for(int i = 0; i < arrNum.size(); i++)
+	{
+		std::cout<<arrNum[i]<<std::endl;
+	}
+}
